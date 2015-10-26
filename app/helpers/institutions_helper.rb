0b00affadaa8a5dda17c0ae5756c29f05798a86c
mphprogrammer <marc@Marys-Air.lan>
2015-10-25 23:51:36 -0400

    Various and sundry

--- app/helpers/institutions_helper.rb
+++ app/helpers/institutions_helper.rb
@@ -6,4 +6,19 @@ module InstitutionsHelper
   def to_bool(val)
     %w(yes true t 1).include?(val.to_s)
   end
+
+  def is_number?(v)
+    v =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
+  end
+
+  def format_number_or_null(v, what) 
+    if is_number?(v)
+      what = v == 1 ? what : what.pluralize
+      number_with_delimiter(v) + " " + what
+    elsif v.blank?
+      pluralize(0, what)
+    else
+      what + " #{v}"
+    end
+  end
 end
