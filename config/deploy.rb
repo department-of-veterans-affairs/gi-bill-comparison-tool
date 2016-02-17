a1b251a08fca78b94cceb82735a13cd5a9db48ff
mphprogrammer <marc.harbatkin.va@gmail.com>
2016-02-17 14:26:39 -0500

    Typo in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :tmp_dir, '/home/ec2-user/tmp'
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml'))
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 #set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
